e6fcd4bf7abe73bdb9bb9d44e875e0535d374355
Harshil Jani <harshiljani2002@gmail.com>
2022-07-28 01:10:01 +0530

    Try deployment

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import { FieldList, FieldCreate, FieldEdit } from "./components/fields.js";
 import buildGraphQLProvider from 'ra-data-graphql-simple';
 
 function App() {
-  const dataProviderType = "GraphQl".toUpperCase();
-  const entities_array = "Student,Teacher,Institute".split(",");
+  const dataProviderType = "REST".toUpperCase();
+  const entities_array = "Student,Teacher".split(",");
   const [dataProvider, setDataProvider] = React.useState(null);
   React.useEffect(() => {
     buildGraphQLProvider({ clientOptions: { uri: 'http://localhost:8000' } })
